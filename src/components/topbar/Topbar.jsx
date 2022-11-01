30768df9c68a3a55fd89d451cfdc1e812d3e904b
Adithya Thejas <adithya.thejas.s@gmail.com>
2022-11-01 20:43:58 +0530

    modified-topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 import "./topbar.css"
-import {Search, Person} from "@mui/icons-material"
+import { Search, Person, Chat, Notifications } from "@mui/icons-material"
 
 export default function Topbar() {
-    return(
+    return (
         <div className="topbarContainer">
             <div className="topbarLeft">
                 <span className="logo">NewSocial</span>
             </div>
             <div className="topbarCentre">
                 <div className="searchbar">
-                    <Search/>
+                    <Search className="searchIcon"/>
                     <input placeholder="Search for friends , Post or any video" className="searchInput" />
                 </div>
             </div>
@@ -21,10 +21,19 @@ export default function Topbar() {
                 </div>
                 <div className="topbarIcons">
                     <div className="topbarIconItem">
-                        <Person/>
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
+                <img src="/assets/persons/1.jpg" alt="" className="topbarImg" />
             </div>
         </div>
     )
