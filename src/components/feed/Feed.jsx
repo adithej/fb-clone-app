209c2e95ec77ef97f7d7ba826a702134916cd296
Adithya Thejas <adithya.thejas.s@gmail.com>
2022-11-06 10:14:15 +0530

    added-shareComponent

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./feed.css"
+import Share from '../share/Share'
 
 export default function Feed() {
   return (
     <div className='feed'>
-      feed
+      <div className="feedWrapper">
+        <Share/>
+      </div>
     </div>
   )
 }
