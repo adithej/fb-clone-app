cb39fbfa41e27f93353b75d4c25b19c04fc91407
Adithya Thejas <adithya.thejas.s@gmail.com>
2022-11-13 10:58:29 +0530

    Readme- edits-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Home from "./pages/home/Home.jsx";
-// import Profile from "./pages/profile/Profile.jsx";
+// import Home from "./pages/home/Home.jsx";
+import Profile from "./pages/profile/Profile.jsx";
 // import Login from "./pages/login/Login.jsx";
 // import Register from "./pages/register/Register.jsx";
 
 function App() {
-  return <Home/>
+  return <Profile/>
 }
 
 export default App;
